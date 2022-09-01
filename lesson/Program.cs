8c626d88e37a69362f113e5fb6b7c4155f4d0ac7
muryshevamarina <marilz@bk.ru>
2022-09-01 22:20:38 +0500

    arkans of birth

---
+++ lesson/Program.cs
@@ -0,0 +1,21 @@
+﻿// int num = 6529;
+// string resalt=Convert.ToString(num);
+
+// int sum =(int) char.GetNumericValue(resalt[0]);
+// Console.WriteLine(sum);
+
+// 27 задача
+Console.WriteLine("Введите число");
+int num = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine(GetSum(num));
+
+int GetSum(int number)
+{
+    int sum = 0;
+    while(number>0)
+    {
+        sum+=number%10;
+        number/=10;
+    }
+    return sum;
+}
